25ea0c8381fdc4f74830d40c97f03b663383f7ad
Korneliusz Rabczak <nikom1337@gmail.com>
2015-10-27 19:58:36 +0100

    store state of application and replicate in cluster while starting new node

--- src/main/java/com/thecookiezen/chat/SimpleChat.java
+++ src/main/java/com/thecookiezen/chat/SimpleChat.java
@@ -5,11 +5,18 @@ import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
 import org.jgroups.View;
+import org.jgroups.util.Util;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 
 @Log4j
 public class SimpleChat extends ReceiverAdapter {
@@ -19,6 +26,7 @@ public class SimpleChat extends ReceiverAdapter {
     private final int nodeId;
     private final String clusterName;
     private final String userName;
+    private final List<String> history = new LinkedList<>();
 
     public SimpleChat(JChannel channel, int nodeId, String clusterName, String userName) {
         this.channel = channel;
@@ -38,6 +46,7 @@ public class SimpleChat extends ReceiverAdapter {
     public void run() throws Exception {
         channel.receiver(this);
         channel.connect(clusterName);
+        channel.getState(null, 10000);
         eventLoop();
         channel.close();
     }
@@ -50,6 +59,10 @@ public class SimpleChat extends ReceiverAdapter {
                 String line = in.readLine().toLowerCase();
                 if (line.startsWith("quit") || line.startsWith("exit"))
                     break;
+                if (line.startsWith("history")) {
+                    System.out.println(history);
+                    continue;
+                }
                 line = "[" + userName + "] " + line;
                 Message msg = new Message(null, null, line);
                 channel.send(msg);
@@ -63,11 +76,30 @@ public class SimpleChat extends ReceiverAdapter {
 
     @Override
     public void receive(Message msg) {
-        System.out.println(msg.getSrc() + " : " + msg.getObject());
+        String message = msg.getSrc() + " : " + msg.getObject();
+        System.out.println(message);
+        synchronized (history) {
+            history.add(message);
+        }
     }
 
     @Override
     public void viewAccepted(View view) {
         System.out.println("new view : " + view);
+    }
+
+    @Override
+    public void getState(OutputStream output) throws Exception {
+        Util.objectToStream(history, new DataOutputStream(output));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void setState(InputStream input) throws Exception {
+        List<String> historyFromCluster = (List<String>) Util.objectFromStream(new DataInputStream(input));
+        synchronized (history) {
+            history.clear();
+            history.addAll(historyFromCluster);
+        }
     }
 }
